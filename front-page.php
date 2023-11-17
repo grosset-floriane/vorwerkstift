905a1294656020dd826d69ffa7ad4f95e126dab9
Floriane Grosset <fl.grosset@gmail.com>
2023-11-17 14:55:58 +0100

    🔥 remove page

--- front-page.php
+++ front-page.php
@@ -45,49 +45,6 @@ get_header();
             </div>
             
         </section>
-
-	
-			<?php
-			while ( have_posts() ) :
-				the_post();
-		
-				/* Check which section the page belongs to display the  
-				** corresponding title in the current language and 
-				** subnavigation.
-				*/
-	
-				// get all the section categories
-				$currentSection = vorwerkstift_get_section_category(null);
-				
-				if(!empty($currentSection)) {
-					$menuID = $currentSection["menuID"];
-					$sectionTitle = $currentSection["sectionTitle"];
-	
-					if($menuID) {
-						?>
-						<header>
-							<p class="section-title" id="section-title"><?php echo $sectionTitle; ?></p>
-							<nav class="sub-navigation">
-								<?php
-									wp_nav_menu(
-										array(
-											'theme_location' => $menuID,
-											'menu_id'        => $menuID,
-										)
-									);
-								?>
-							</nav>
-							</header>
-						<?php
-					}
-				}
-	
-				get_template_part( 'template-parts/content', 'page' );
-	
-			endwhile; // End of the loop.
-			?>
-			
-	
 		</main><!-- #main -->
 	
 		<aside id="sidebar-desktop" class="widget-area">
