a3bcf0581bb89bbac72530337753e7db2f5b4dfb
Floriane Grosset <fl.grosset@gmail.com>
2024-01-18 15:10:36 +0100

    🐛 fix auto translate on German must text and site title

--- components/site-branding/site-branding.php
+++ components/site-branding/site-branding.php
@@ -14,11 +14,11 @@
    */
   ?>
 <div class="site-branding">
-    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><span></span></a></p>
+    <p class="site-title" translate="no" lang="de"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><span></span></a></p>
     <?php
         $vorwerkstift_description = get_bloginfo( 'description', 'display' );
         if ( $vorwerkstift_description || is_customize_preview() ) :
     ?>
-        <p class="site-description"><?php echo $vorwerkstift_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+        <p class="site-description" translate="no" lang="de"><?php echo $vorwerkstift_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
     <?php endif; ?>
 </div><!-- .site-branding -->
