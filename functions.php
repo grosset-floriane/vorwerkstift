d9af619baf947de145415df8f5f3d7ccd6764edb
Floriane Grosset <fl.grosset@gmail.com>
2024-01-26 13:46:56 +0100

    ✨ add styles and accessibility for uploaded files

--- functions.php
+++ functions.php
@@ -172,6 +172,7 @@ function vorwerkstift_scripts() {
 
 	wp_enqueue_script( 'vorwerkstift-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_script( 'vorwerkstift-styles', get_template_directory_uri() . '/js/styles.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'vorwerkstift-accessibility', get_template_directory_uri() . '/js/accessibility.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ js/accessibility.js
@@ -0,0 +1,38 @@
+/**
+ * File accessibility.js.
+ *
+ * Handles add various accessibility cues
+ * - add indication of opening tabs in new link
+ */
+const OPEN_TEXT = {
+  de: "öffnet sich in einem neuen Tab",
+  en: "opens in a new tab"
+};
+
+(function () {
+  // Indicate a link is going to open in a new tab
+  // Add noopener to prevent vulnerability
+  function addNoOpener(link) {
+    const linkTypes = (link.getAttribute("rel") || "").split(" ");
+    if (!linkTypes.includes("noopener")) {
+      linkTypes.push("noopener");
+    }
+    link.setAttribute("rel", linkTypes.join(" ").trim());
+  }
+
+  const lang = document.location.href.split("/")[3];
+
+  function addNewTabMessage(link) {
+    if (!link.querySelector(".screen-reader-only")) {
+      link.insertAdjacentHTML(
+        "beforeend",
+        ` <span class="screen-reader-only">(${OPEN_TEXT[lang]})</span>`
+      );
+    }
+  }
+
+  document.querySelectorAll('a[target="_blank"]').forEach((link) => {
+    addNoOpener(link);
+    addNewTabMessage(link);
+  });
+})();
